3cf4d6bb5058a83e92cb22d25c44df26100863f3
Darlin <darlin.estrada456@gmail.com>
2026-01-22 20:21:04 -0500

    feat se crea diccionario

---
+++ main.py
@@ -0,0 +1,17 @@
+
+contador=0
+'''while contador<5:
+    contador=contador+1
+    print(f"Estoy en la vuelta: {contador}")'''
+    
+
+for _ in range(1,6):
+     diccionario={}
+     diccionario["id"]=int(input("Digita el id del empleado: "))
+     diccionario["nombres"]=input("Digita los nombres del empleado: ")
+     diccionario["documento"]=input("digita el documento de identidad del empleado: ")
+     diccionario["correo"]=input("digita el correo del empleado: ")
+     diccionario["contraseña"]=input("digita la contraseña: ")
+
+
+
